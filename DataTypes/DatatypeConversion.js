c4d4f38051c438674588973456ff9293ae41cd68
Prachi Dubey <006prachi@gmail.com>
2026-04-26 13:55:52 +0530

    DatatypeConversion

--- DataTypes/DatatypeConversion.js
+++ DataTypes/DatatypeConversion.js
@@ -57,6 +57,10 @@ let value = 3
 let negValue = -value
 // console.log(negValue);
 
+
+
+
+
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
@@ -64,29 +68,59 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
+
+
+
+
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " prachi"
 
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+
+
+
+
+// console.log("1" + 2);          //12
+// console.log(1 + "2");          //12
+// console.log("1" + 2 + 2);      //122
+// console.log(1 + 2 + "2");      //32
+
+
+
+
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+
+
+
+
+
+//Not A Good Practice
+// console.log(+true);            //1
+// console.log(+"");              //0
+
+
+
+
 
 let num1, num2, num3
-
 num1 = num2 = num3 = 2 + 2
+
+
+
+
 
 let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
+
+
+
+
+
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
