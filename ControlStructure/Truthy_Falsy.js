fb2f94c4f23513d06cae19ae9f81ca5771b75505
Prachi Dubey <006prachi@gmail.com>
2026-05-23 12:35:38 +0530

    Control flow

---
+++ ControlStructure/Truthy_Falsy.js
@@ -0,0 +1,58 @@
+const userEmail = []
+//const userEmail = "prachi@.ai"
+//const userEmail = ""
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+//also all the values that are not falsy
+// "0", 'false', " ", [], {}, function(){}
+
+
+
+
+
+//for checking empty array
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+
+
+
+const emptyObj = {}
+
+    //returns an array of key obj
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10              //5
+// val1 = null ?? 10           //10
+// val1 = undefined ?? 15      //15
+val1 = null ?? 10 ?? 20        //10 
+
+console.log(val1);
+
+
+
+
+
+// Terniary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
