31e016bebc97edc6c70b26f5dfc84b1013772227
Prachi Dubey <006prachi@gmail.com>
2026-05-29 18:42:22 +0530

    Color Switcher

---
+++ Projects/01ColorSwitcher/index.js
@@ -0,0 +1,37 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+//NodeList
+buttons.forEach(function (button) {
+  button.addEventListener('click', function (e) {
+
+    if (e.target.id === 'pink') {
+      body.style.backgroundColor = '#FBCFE8';
+    }
+
+    if (e.target.id === 'purple') {
+      body.style.backgroundColor = '#E9D5FF';
+    }
+
+    if (e.target.id === 'beige') {
+      body.style.backgroundColor = '#F5F5DC';
+    }
+
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = '#FEF9C3';
+    }
+
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = '#BFDBFE';
+    }
+
+    if (e.target.id === 'green') {
+      body.style.backgroundColor = '#BBF7D0';
+    }
+
+    if (e.target.id === 'brown') {
+      body.style.backgroundColor = '#D2B48C';
+    }
+
+  });
+});
